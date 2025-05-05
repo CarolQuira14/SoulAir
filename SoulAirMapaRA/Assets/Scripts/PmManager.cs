f2da585a98e26e70ed41486fdfe98f78bd5f4243
CarolQuira14 <carolquira14@gmail.com>
2025-05-05 13:33:11 -0500

    Añadir Lean Touch
    Conjunto de assets necesaarios para la interaccion con las particulas en RA

--- SoulAirMapaRA/Assets/Scripts/PmManager.cs
+++ SoulAirMapaRA/Assets/Scripts/PmManager.cs
@@ -1,22 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 public class PmManager : MonoBehaviour
 {
-    public GameObject particlePrefab;
-    public int particleCount = 20;
+    public GameObject objPrefab;
+    private GameObject spawnedObject;
+    private Pose pose;
+    [SerializeField] private ARRaycastManager aRRaycastManager;
+    private bool poseEsValida = false;
+
     void Start()
     {
-        for (int i = 0; i < particleCount; i++){
-            Vector3 randomPosition = Camera.main.transform.position + Random.onUnitSphere * Random.Range(0.3f, 1.0f);
-            Instantiate(particlePrefab, randomPosition, Quaternion.identity);
+        aRRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    // Se usa para actualizar la pose y ponerla en el plano 
+    void Update()
+    {
+        ActualizarPose();
+
+        if (spawnedObject == null && poseEsValida)
+        {
+            Debug.Log("Superficie detectada, colocando objeto automáticamente.");
+            PonerObjetoEnPlano();
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void ActualizarPose()
     {
-        
+        var pantallaCentro = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        aRRaycastManager.Raycast(pantallaCentro, hits, TrackableType.Planes);
+
+        poseEsValida = hits.Count > 0;
+
+        if (poseEsValida)
+        {
+            pose = hits[0].pose;
+        }
+
     }
+    void PonerObjetoEnPlano()
+    {
+        spawnedObject = Instantiate(objPrefab, pose.position, pose.rotation);
+    }
+
+    /* public GameObject particlePrefab;
+     public int particleCount = 20;
+     void Start()
+     {
+         for (int i = 0; i < particleCount; i++){
+             Vector3 randomPosition = Camera.main.transform.position + Random.onUnitSphere * Random.Range(0.3f, 1.0f);
+             Instantiate(particlePrefab, randomPosition, Quaternion.identity);
+         }
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+
+     }*/
 }
